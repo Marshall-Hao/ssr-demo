06d363e50e52ab1d24e830440569b7bfeb74a769
Marshall Chan <marshallchan666@outlook.com>
2022-06-14 05:49:30 +0800

    feat: getData with match path for different ssr routes, use type.getData to get the fn hooked

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { fetchUser } from "../core/api";
+import { dataContext } from "../core/context";
 
 const User = () => {
+  const context = useContext(dataContext);
+  console.log("dataContext", context);
+
+  useEffect(() => {
+    fetchUser().then((data) => console.log(data));
+  }, []);
+
   return (
     <main>
       <h1>User Page</h1>
--- src/server/appRouter.js
+++ src/server/appRouter.js
@@ -3,16 +3,27 @@ import React from "react";
 import ReactDomServer from "react-dom/server";
 import Document from "../components/document";
 import App from "../components/app";
+import { matchPath } from "react-router-dom";
 import { StaticRouter } from "react-router-dom/server";
 import { fetchUser } from "../core/api";
 import { dataContext } from "../core/context";
+import routes from "../core/routes";
 
 const router = express.Router();
 
 router.get("*", async (req, res) => {
   let data = {};
+  let getData = null;
+  routes.some((route) => {
+    const match = matchPath(req.path, route.path);
+    if (match) {
+      getData = route.element.type.getData || {};
+    }
+    return match;
+  });
+  // if (typeof getData === "function") {
   try {
-    data = await fetchUser();
+    data = await getData();
     const appString = await ReactDomServer.renderToString(
       <StaticRouter location={req.url}>
         <dataContext.Provider value={data}>
@@ -26,6 +37,7 @@ router.get("*", async (req, res) => {
     );
     res.send(html);
   } catch (e) {}
+  // }
 });
 
 module.exports = router;
