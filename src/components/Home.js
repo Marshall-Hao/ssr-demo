75cc4fc68253e45789aaebf34cb308a6bdad2c12
Marshall Chan <marshallchan666@outlook.com>
2022-06-14 16:59:20 +0800

    feat: data rendering with fix of core.js package

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useContext } from "react";
+import React, {
+  useEffect,
+  useContext,
+  useState,
+} from "react";
 import { fetchHome } from "../core/api";
 import { dataContext } from "../core/context";
 
 const Home = () => {
-  const context = useContext(dataContext);
+  const context = useContext(dataContext) || {};
   console.log("dataContext", context);
+  const [home, setHome] = useState(context);
   // * 类似didmount
   //   *https://www.ruanyifeng.com/blog/2020/09/react-hooks-useeffect-tutorial.html
   useEffect(() => {
-    fetchHome().then((data) => console.log(data));
+    fetchHome().then((data) => {
+      setHome(data);
+    });
   }, []);
 
   return (
     <main>
       <h1>Home Page</h1>
+      <h1>{home.title}</h1>
+      <p>{home.desc}</p>
     </main>
   );
 };
